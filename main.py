5f96f2c28e82d0171a26df00f8715a1f1a873696
sidith <58284168+sidith@users.noreply.github.com>
2024-10-23 17:41:17 +0900

    added the count_words func

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('howdy')
+book_path = "books/frankenstein.txt"
+
+
+def main():
+    word_count = 0
+    content = ""
+
+    with open(book_path) as file:
+       content = file.read()
+
+    words = content.split()
+
+    count_words(words)
+
+def count_words(words):
+    print(len(words))
+
+main()
